402bb79896e92dd9ee2dcb32b357cb30804dff09
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-04 13:42:00 +0200

    added snack-4 folder

--- snack-3/index3.php
+++ snack-3/index3.php
@@ -35,17 +35,19 @@ $db = [
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style3.css">
 </head>
 
 <body>
     <main>
         <div class="container">
-            <div class="teachers-box">
+            <div class="teachers-box mt-5">
                 <ul>
-                    <?php foreach { ?>
-                    <li>
-
-                    </li>
+                    <?php foreach ($db as $key => $teacher) { ?>
+                        <li class="text-white">
+                            <?php echo $key; ?>
+                        </li>
                     <?php } ?>
                 </ul>
             </div>
